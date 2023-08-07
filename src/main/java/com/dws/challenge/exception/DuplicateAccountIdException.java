3b9fbe4db62e1f2227d79900330498d09d6d1ecf
suryaambati <70249657+suryaambati@users.noreply.github.com>
2023-08-07 16:51:38 +0530

    Delete src directory

--- src/main/java/com/dws/challenge/exception/DuplicateAccountIdException.java
+++
@@ -1,8 +0,0 @@
-package com.dws.challenge.exception;
-
-public class DuplicateAccountIdException extends RuntimeException {
-
-  public DuplicateAccountIdException(String message) {
-    super(message);
-  }
-}
